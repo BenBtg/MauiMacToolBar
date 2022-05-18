a185dab230f274f68bae4a31391f1950e7eea499
Ben Buttigieg <benbtg@gmail.com>
2022-05-18 14:14:42 +0100

    Added Custom Toolbar (required negative margin)

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,9 @@
-﻿namespace MauiMacToolBar;
+﻿using System.Diagnostics;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using UIKit;
+
+namespace MauiMacToolBar;
 
 public partial class App : Application
 {
@@ -6,6 +11,29 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
+		//MainPage = new AppShell();
+	}
+	
+
+	protected override Window CreateWindow(IActivationState activationState)
+	{
+		var mainWin = new Window();
+		mainWin.HandlerChanged += MainWind_HandlerChanged;
+
+		var rootNav = new MainPage();
+
+		mainWin.Page = rootNav;
+		return mainWin;
+	}
+
+	private void MainWind_HandlerChanged(object sender, EventArgs e)
+	{
+		var window = sender as Microsoft.Maui.Controls.Window;
+		var uiWindow = window.Handler.PlatformView as UIWindow;
+
+		if (uiWindow != null)
+		{
+			uiWindow.WindowScene.Titlebar.TitleVisibility = UITitlebarTitleVisibility.Hidden;
+		}
 	}
 }
